96885fd0f11e5a3ef8dd92c184ba87ffd49161c7
LydiaAlem <107647071+LydiaAlem@users.noreply.github.com>
2022-09-19 09:12:50 -0500

    Create Circle.java
    I kind of just wrote the outline

---
+++ Circle.java
@@ -0,0 +1,45 @@
+import java.lang.Math;
+
+public class Circle{
+    public double xPos, yPos, radius;
+
+    public static void main(String[] args) {
+    
+        
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+//Accessor method 
+
+    }
+    public double CalculatePerimeter(){
+        return (xPos + xPos + yPos + yPos);
+    }
+
+    public double calculateArea() {
+        return(2 * Math.PI * (radius * radius));
+    }
+
+    public double setColor(String color){
+        
+    }
+
+//Methods
+
+    public void
+   
+
+    
+
+
+    
+    
+}
+
