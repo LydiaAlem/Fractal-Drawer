a7a7ade5271b002cb89450335e63668caf773141
LydiaAlem <107647071+LydiaAlem@users.noreply.github.com>
2022-09-19 15:46:30 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,39 +1,35 @@
+//CIRCLE CLASS:
 import java.lang.Math;
 
-public class Circle{
-    public double xPos, yPos, radius;
+public class Circle{;
 
     public static void main(String[] args) {
+        //Accessor Method:
+        calculatePerimeter(5, 5, 2);
+        calculateArea(3);
+    }
+                    
+//METHODS:
+    public static double calculatePerimeter(double xPosition, double yPosition, double radius){
+        return (2 * Math.PI * radius);
+    }
+
+    public static double calculateArea(double radius) {
+        return (Math.PI * (radius * radius));
+    }
+
+    public void setColor(String Color) {
+        return;
+    }
+
+    public double setPos(double xPosition, double yPosition) {
+        this.xPosition = xPosition;
+
+    }
     
-        
-/*
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
+}
 
-//Accessor method 
 
-    }
-    public double CalculatePerimeter(){
-        return (xPos + xPos + yPos + yPos);
-    }
-
-    public double calculateArea() {
-        return(2 * Math.PI * (radius * radius));
-    }
-
-    public double setColor(String color){
-        
-    }
-
-//Methods
-
-    public void
    
 
     
