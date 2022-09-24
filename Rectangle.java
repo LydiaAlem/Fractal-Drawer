d2301608c3c0d1c1f0c9236f7207c740db5e7912
LydiaAlem <107647071+LydiaAlem@users.noreply.github.com>
2022-09-24 00:53:06 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,48 @@
+//Rectangle Class:
+public class Rectangle {
+    double xPosition;
+    double yPosition;
+    double width;
+    double height;
+    Color color;
+
+    public static void main(String[] args) {
+    //
+    }
+
+    //Constructor:
+    public Rectangle(double xPosition, double yPosition, double width, double height){
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double calculatePerimeter(){
+        return (width * 2) + (height * 2);
+    }
+
+    public double calculateArea(){
+        return (width * height);
+    }
+
+    public void setColor(Color color){
+        this.color = color;
+    }
+
+    public void setPos(double xPosition, double yPosition){
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+
+}
+
 
