647e385f56f90798cc633a87496a6e46907bee9d
LydiaAlem <107647071+LydiaAlem@users.noreply.github.com>
2022-09-23 22:56:51 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,33 +1,65 @@
-//CIRCLE CLASS:
+/CIRCLE CLASS:
 import java.lang.Math;
 
-public class Circle{;
+public class Circle{
+    static double xPosition;
+    static double yPosition;
+    static double radius;
+    static Color color;
 
     public static void main(String[] args) {
-        //Accessor Method & Verifying that the functions works:
-        calculatePerimeter(5, 5, 2);
-        calculateArea(3);
+        //calculatePerimeter(5.0);
+        //calculateArea(3);
     }
-                    
-//METHODS:
-    public static double calculatePerimeter(double xPosition, double yPosition, double radius){
+
+//I'm not sure if this is the EXACT way to add the constructor?
+    // public Circle(double xPosition, double yPosition, double radius) {
+    //     this.xPosition = xPosition;
+    //     this.yPosition = yPosition;
+    //     this.radius = radius;
+    // }
+
+    public double calculatePerimeter(double radius){
         return (2 * Math.PI * radius);
     }
 
-    public static double calculateArea(double radius) {
+    public double calculateArea(double radius) {
         return (Math.PI * (radius * radius));
     }
 
-    public void setColor(String Color) {
-        return;
+    public void setColor(Color color) {
+        color = color;
     }
 
-    public double setPos(double xPosition, double yPosition) {
-        this.xPosition = xPosition;
+    public void setPos(double xPosition, double yPosition) {
+        xPosition = xPosition;
+        yPosition = yPosition;
+    }
 
+    public void setRadius(double radius){
+        radius = radius;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public double getXPos(){
+        return xPosition;
     }
     
+    public double getYPos() {
+        return yPosition;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
 }
+
+
+
 
 
    
