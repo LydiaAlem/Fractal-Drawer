e89c8a3be6087942dc03ef8a9a41ac5c93666477
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-01 12:45:40 -0500

    Update FractalDrawer.java
    I’m still not done but this is what I have left!

--- FractalDrawer.java
+++ FractalDrawer.java
@@ -1,10 +1,18 @@
-// FractalDrawer class draws a fractal of a shape indicated by user input
+/ FractalDrawer class draws a fractal of a shape indicated by user input
 import java.util.Scanner;
 import java.awt.Color;
 import java.util.Random;
 public class FractalDrawer {
     static double totalArea=0;  // member variable for tracking the total area
     public FractalDrawer() {}  // contructor
+
+    public static double getArea(){
+        return totalArea;
+    }
+
+    public void setArea(double d) {
+        totalArea = d;
+    }
 
     //TODO:
 
@@ -16,26 +24,22 @@ public class FractalDrawer {
     public static double drawFractal(String type) {
         Random rand = new Random();
         Canvas drawing = new Canvas();
-/*
- * HELP!!! Im trying to get a random color but im a little bit confused on how 
- * to use rand when searching a scope{}????  
- */
-        Color[] userColor = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.PINK, Color.DARK_GRAY};
-        Color rand_color = userColor.get(rand.nextColor(userColor.size()));
 
+        Color[] userColor = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.PINK, Color.DARK_GRAY, Color.cyan};
+        double user = Math.random() * userColor.length;
 
-        if(type.equals("Triangle")){
+        if(type.equals("triangle")){
             Triangle triangle = new Triangle(10, 10, 5, 5);
             drawTriangleFractal(triangle.getXPos(), triangle.getYPos(),triangle.getWidth(), triangle.getHeight(), userColor[-1], drawing, 0) ;
             //
         }
 
-        if(type.equals("Rectangle")){
+        if(type.equals("rectangle")){
             Rectangle rectangle = new Rectangle(10, 10, 5, 5);
             drawRectangleFractal(rectangle.getXPos(), rectangle.getYPos(), rectangle.getWidth(), rectangle.getHeight(), userColor[-1], drawing, 0);
         }
 
-        if(type.equals("Circle")){
+        if(type.equals("circle")){
             Circle circle = new Circle(10, 10, 5);
             drawCircleFractal(circle.getXPos(), circle.getYPos(), circle.getRadius(), userColor[-1], drawing, 0);
         }
@@ -44,11 +48,12 @@ public class FractalDrawer {
 
 
 
-
+        return getArea();
 
 
 
     }
+
     //TODO:
     // drawTriangleFractal draws a triangle fractal using recursive techniques
     public void drawTriangleFractal(double width, double height, double x, double y, Color c, Canvas can, int level){
@@ -57,15 +62,15 @@ public class FractalDrawer {
             can.drawShape(new_Triangle);
         }
         else {
-            Color[] color = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.PINK, Color.DARK_GRAY};
+            Color[] userColor = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.PINK, Color.DARK_GRAY};
+            double user = Math.random() * userColor.length;
             Triangle new_Triangle = new Triangle(width, height, x, y);
-            
+            drawTriangleFractal(width/2, height/2, x/2, y/2, user, can, level + 1);
+            drawTriangleFractal(width/2, height/2, x/2, y/2, user, can, level + 1);
+            drawTriangleFractal(width/2, height/2, x/2, y/2, user, can, level + 1);
+            setArea(getArea() + new_Triangle.calculateArea());
         }
-
-
     }
-
-
 
 
 
@@ -84,7 +89,7 @@ public class FractalDrawer {
     // should print area of fractal
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Pick: Circle, Rectangle or Triangle: ");
+        System.out.println("Pick: circle, rectangle or triangle: ");
         String type = input.nextLine();
 
         drawFractal(type);
